b8067b54e876dfcba879782266aa50e8a24a88f5
Arne Vogel <arnevogel96@gmail.com>
2019-04-22 12:30:56 +0200

    warn users client side about short passwords

--- server/static/js/register.js
+++ server/static/js/register.js
@@ -1,9 +1,17 @@
 function validate() {
     var user = document.getElementsByName("username")[0].value;
+    var pass = document.getElementsByName("password")[0].value;
     if (user.indexOf('@') > -1) {
         alert("Don't use an email as username.");
         event.preventDefault();
         return false;
+    }
+    if (pass.length < 6) {
+        cont = confirm("Your password length seems to be under 6 characters, please confim that you want to use this possibly unsecure password.")
+        if (!cont) {
+            event.preventDefault();
+            return false;
+        }
     }
     return true;
 }
